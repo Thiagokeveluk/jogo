d33c37b8a2d6c379398ff2675970489538ae9f5c
Thiagokeveluk <thiagokeveluk85@gmail.com>
2025-10-07 10:25:04 -0300

    add de tudo

---
+++ script.js
@@ -0,0 +1,54 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const tempoSpan = document.getElementById('tempo');
+const recordeSpan = document.getElementById('recorde');
+
+let tempo = 0;
+let recorde = localStorage.getItem('recorde') || 0;
+recordeSpan.textContent = recorde;
+
+let cronometro = setInterval(() => {
+    tempo++;
+    tempoSpan.textContent = tempo;
+}, 900);
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+};
+
+const loop = setInterval(() => {
+
+    console
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 130 && pipePosition > 0 && marioPosition < 100 ) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+       mario.src ='./img/game-over.png';
+       mario.style.width ='70px';
+       mario.style.marginLeft = '60px';
+
+
+       clearInterval(loop);
+       clearInterval(cronometro);
+
+       if( tempo > recorde) {
+        localStorage.setItem('recorde', tempo);
+        recordeSpan.textContent = tempo;
+       }
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
